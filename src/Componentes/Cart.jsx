6e2e7b52aec2cfb224392bf57d4888368395f221
Camilo <cmonsalsilva@gmail.com>
2025-12-05 23:03:52 -0300

    Se agregan estilos a Cart.jsx, se agrega helper

--- src/Componentes/Cart.jsx
+++ src/Componentes/Cart.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {pizzaCart} from '../pizzas.js'
 import { useState } from 'react'
+import {formatNumber} from "../utils/format";
+
 
 const Cart = () => {
     const [cart, setCart] = useState(pizzaCart);
@@ -17,16 +19,21 @@ const Cart = () => {
     const total = cart.reduce((acc, pizza) => acc + pizza.price * pizza.count, 0);
   
     return (
-    <div className="cart-container my-5">
-        <h4>Detalles del pedido:</h4>
+
+    <div className="cart-container d-flex flex-column justify-content-around  align-items-center p-4 mt-4 p-5 bg-light">
+        <h4 className="mb-3 ">Detalles del pedido:</h4>
         <ul className='list-group'>
           {cart.map((pizza) => (
-           <li key={pizza.id} className="list-group-item d-flex justify-content-between align-items-center">
+           <li key={pizza.id} className="list-group-item d-flex justify-content-between ">
             <div>
-              <img src={pizza.img} alt={pizza.name} width="60" className="me-2" />
-              <strong className="text-capitalize">{pizza.name}</strong> - ${pizza.price}
+              <img src={pizza.img} alt={pizza.name} width="70" className="me-3" />
+              <strong className="text-capitalize me-5">{pizza.name}</strong>
+
             </div>
-            <div>
+
+            
+            <div className="d-flex align-items-center">
+              <span className="me-2"> ${formatNumber(pizza.price)}</span> 
               <button className="btn btn-sm btn-success me-2" onClick={() => increase(pizza.id)}>+</button>
               <button className="btn btn-sm btn-danger me-2" onClick={() => decrease(pizza.id)}>-</button>
               Cantidad: {pizza.count}
@@ -34,8 +41,8 @@ const Cart = () => {
           </li>
           ))}
         </ul>
-         <h5 className="mt-3">Total: ${total}</h5>
-         <button className="btn btn-primary mt-2">Pagar</button>
+         <h5 className="mt-3 mb-3">Total: $ {formatNumber(total)}</h5>
+         <button className="btn btn-primary mb-4">Pagar</button>
     
     </div>
     )
